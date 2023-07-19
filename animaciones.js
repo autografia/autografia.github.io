9f93ac215ccc7c503bd3190ab8a78baab40aa224
autografia <player.navas@gmail.com>
2023-07-19 18:14:01 +0200

    limpieza de codigo redundado y nueva forma de generar nombres imagenes hr

--- animaciones.js
+++ animaciones.js
@@ -8,18 +8,23 @@ $(window).on('load', function () {
   $("#loader-wrapper").fadeOut(700);
 });
 
-const randomBar = document.getElementById("random-bar");
+/*const randomBar = document.getElementById("random-bar");
 const fondos = ["hr/hr_1.png","hr/hr_2.png","hr/hr_3.png", "hr/hr_4.png", "hr/hr_5.png", "hr/hr_6.png", "hr/hr_7.png", "hr/hr_8.png", "hr/hr_9.png", "hr/hr_10.png","hr/hr_11.png", "hr/hr_12.png", "hr/hr_13.png", "hr/hr_14.png", "hr/hr_15.png", "hr/hr_16.png", "hr/hr_17.png", "hr/hr_18.png", "hr/hr_19.png", "hr/hr_20.png", "hr/hr_21.png", "hr/hr_22.png", "hr/hr_23.png", "hr/hr_24.png", "hr/hr_25.png", "hr/hr_26.png", "hr/hr_27.png", "hr/hr_28.png"];
 
+const randomImage = fondos[Math.floor(Math.random() * fondos.length)];*/
+
+const randomBar = document.getElementById("random-bar");
+const cantidadImagenes = 28;
+const fondos = [];
+
+// Generar los nombres de las imágenes automáticamente
+for (let i = 1; i <= cantidadImagenes; i++) {
+  fondos.push(`hr/hr_${i}.png`);
+}
+
 const randomImage = fondos[Math.floor(Math.random() * fondos.length)];
-randomBar.style.backgroundImage = `url(${randomImage})`;
 
-
-
-
-
-
-
+//console.log(randomImage)
 
 //De aqui para abajo es el lazy loading para que no tarde en cargar
 
--- modal.js
+++ modal.js
@@ -1,61 +1,63 @@
 const laFuncionQueSea = () => {
- // Selecciona todas las imágenes de la galería
-const galleryImages = document.getElementsByClassName('photo')[0].getElementsByTagName('img');
 
-
-// Selecciona la ventana emergente, la imagen dentro de ella y el botón de descarga
-const modal = document.querySelector('.modal');
-const modalImg = document.querySelector('.modal-content');
-const modalDownloadButton = document.querySelector('#download-button');
-
-// Agrega el evento de clic al botón de descarga
-modalDownloadButton.addEventListener('click', () => {
-  window.location.href = modalDownloadButton.href;
-});
-
-// Agrega un evento de clic a cada imagen
-const gallery = document.getElementsByClassName('photo')[0];
-gallery.addEventListener('click', (event) => {
-  if (event.target.tagName === 'IMG') {
-    modal.style.display = 'block';
-    modalImg.src = event.target.dataset.highres;
-    modalDownloadButton.href = event.target.dataset.highres;
-    captionText.innerHTML = event.target.alt;
-  }
-});
-
-// Agrega un evento de clic al botón de cerrar la ventana emergente
-const closeBtn = document.getElementsByClassName('close')[0];
-if (closeBtn) {
-  closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
+  //Prueba de codigo redundado a eliminar  
+  // Agrega un evento de clic a cada imagen
+  /*const gallery = document.getElementsByClassName('photo')[0];
+  gallery.addEventListener('click', (event) => {
+    if (event.target.tagName === 'IMG') {
+      modal.style.display = 'block';
+      modalImg.src = event.target.dataset.highres;
+      modalDownloadButton.href = event.target.dataset.highres;
+      captionText.innerHTML = event.target.alt;
+    }
   });
-}
+  
 
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var images = document.querySelectorAll(".photo img");
-var captionText = document.querySelector(".caption");
-
-// Loop through all images and add the click event listener
-for (var i = 0; i < images.length; i++) {
-  images[i].addEventListener("click", function() {
-    modal.style.display = "block";
-    modalImg.src = this.dataset.highres;
-    modalDownloadButton.download = this.alt;
-    modalDownloadButton.href = this.dataset.highres;
-    captionText.innerHTML = this.alt;
+  //Prueba de codigo redundado a eliminar
+  // Agrega el evento de clic a la ventana emergente para cerrarla al hacer clic fuera de ella
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
   });
-}
+
+  
+  */
 
 
+  // Selecciona todas las imágenes de la galería
+  const galleryImages = document.getElementsByClassName('photo')[0].getElementsByTagName('img');
+    // Selecciona la ventana emergente, la imagen dentro de ella y el botón de descarga
+  const modal = document.querySelector('.modal');
+  const modalImg = document.querySelector('.modal-content');
+  const modalDownloadButton = document.querySelector('#download-button');
 
-// Agrega el evento de clic a la ventana emergente para cerrarla al hacer clic fuera de ella
-window.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.style.display = 'none';
+  // Agrega el evento de clic al botón de descarga
+  modalDownloadButton.addEventListener('click', () => {
+    window.location.href = modalDownloadButton.href;
+  });
+
+  // Agrega un evento de clic al botón de cerrar la ventana emergente
+  const closeBtn = document.getElementsByClassName('close')[0];
+  if (closeBtn) {
+    closeBtn.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
   }
-}); 
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var images = document.querySelectorAll(".photo img");
+  var captionText = document.querySelector(".caption");
+
+  // Agrega un evento de clic a cada imagen
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", function() {
+      modal.style.display = "block";
+      modalImg.src = this.dataset.highres;
+      modalDownloadButton.download = this.alt;
+      modalDownloadButton.href = this.dataset.highres;
+      captionText.innerHTML = this.alt;
+    });
+  } 
 }
 
 fetch('/fotos/moralet_2023/fotos.json')
@@ -84,8 +86,8 @@ fetch('/fotos/moralet_2023/fotos.json')
 
 // Agrega la función imgError
 function imgError() {
-this.onerror = "";
-this.src = "/logos/error404.gif";
-return true;
+  this.onerror = "";
+  this.src = "/logos/error404.gif";
+  return true;
 }
 
