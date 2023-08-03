0f3421941194dc9e486771787fff1d081069cdef
Autografia <130606021+autografia@users.noreply.github.com>
2023-08-03 13:12:51 +0200

    Update modal.js

--- modal.js
+++ modal.js
@@ -48,30 +48,7 @@ const laFuncionQueSea = () => {
       
       captionText.innerHTML = this.alt;
     });
-  }
-/* -----------------------------------------
-  Have focus outline only for keyboard users 
- ---------------------------------------- */
-
-const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
-
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
-  }
-
-}
-
-const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
-
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
-}
-
-window.addEventListener('keydown', handleFirstTab)
-    
+  }   
 }
 
 // Obtiene el nombre del archivo HTML actual
