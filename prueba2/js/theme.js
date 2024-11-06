b5b098007ef7651e081869d267c978da89414019
autografia <autografia.fotografia@gmail.com>
2024-11-06 16:30:08 +0100

    add change color on card title

--- prueba2/js/theme.js
+++ prueba2/js/theme.js
@@ -2,12 +2,15 @@ function toggleTheme() {
     if (document.body.classList.contains('light-theme')) {
         document.body.classList.remove('light-theme');
         document.body.classList.add('dark-theme');
+
         const themeIcon = document.getElementById('themeToggle').querySelector('.theme-icon');
         themeIcon.classList.remove('bi-moon-stars-fill');
         themeIcon.classList.add('bi-sun-fill');
+        
     } else {
         document.body.classList.remove('dark-theme');
         document.body.classList.add('light-theme');
+
         const themeIcon = document.getElementById('themeToggle').querySelector('.theme-icon');
         themeIcon.classList.remove('bi-sun-fill');
         themeIcon.classList.add('bi-moon-stars-fill');
