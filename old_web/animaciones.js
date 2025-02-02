b11f2c88a133c17f34b85acf694071842d9fa7ff
autografia <autografia.fotografia@gmail.com>
2025-02-02 20:45:05 +0100

    remove old pics and website to the new web

---
+++ old_web/animaciones.js
@@ -0,0 +1,60 @@
+window.onload = function () {
+  setTimeout(function () {
+    document.getElementById("fadein").remove();
+  }, 1000);
+};
+
+$(window).on('load', function () {
+  $("#loader-wrapper").fadeOut(700);
+});
+
+
+const randomBar = document.getElementById("random-bar");
+const cantidadImagenes = 28;
+const fondos = [];
+// Generar los nombres de las imágenes automáticamente
+for (let i = 1; i <= cantidadImagenes; i++) {
+  fondos.push(`old_web/hr/hr_${i}.png`);
+}
+
+const randomImage = fondos[Math.floor(Math.random() * fondos.length)];
+randomBar.style.backgroundImage = `url(${randomImage})`;
+
+
+
+
+//De aqui para abajo es el lazy loading para que no tarde en cargar
+
+// Función para cargar la imagen en segundo plano y asignar la propiedad src
+const loadImage = (image) => {
+  const src = image.dataset.highres;
+  const img = new Image();
+  img.src = src;
+  img.onload = () => {
+    image.src = src;
+  };
+};
+
+// Función para verificar si la imagen está en la pantalla
+const isInView = (element) => {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+/*
+// Selecciona todas las imágenes de la galería
+const galleryImages = document.getElementsByClassName('photo')[0].getElementsByTagName('img');
+
+// Carga las imágenes en segundo plano a medida que se muestran en pantalla
+window.addEventListener('scroll', () => {
+  galleryImages.forEach(image => {
+    if (isInView(image) && !image.src) {
+      loadImage(image);
+    }
+  });
+});
+*/
---
+++ old_web/modal2.js
@@ -0,0 +1,103 @@
+// Función para manejar errores de imagen y mostrar el logo durante la carga
+const imgError = (image) => {
+  const logo = document.createElement('img');
+  logo.src = 'logos/logo.png'; // Ruta a tu logo
+  logo.alt = 'Autografia Logo'; // Alt para accesibilidad
+  logo.classList.add('loading-logo'); // Agregar clase para estilos CSS
+  
+  // Reemplazar la imagen que falló con el logo
+  image.parentNode.replaceChild(logo, image);
+
+  // Si hay un error en la carga de la imagen, muestra un mensaje de error en la consola
+  console.error('Error al cargar la imagen:', image.src);
+};
+
+// Función para cargar fotos desde un JSON y agregarlas a una galería
+const cargarFotos = (json, galeria) => {
+  return fetch(json)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(foto => {
+        const div = document.createElement('div');
+        div.classList.add('photo');
+
+        const img = document.createElement('img');
+        img.src = foto.baja_calidad; // Solo se carga la imagen de baja calidad inicialmente
+        img.dataset.highres = foto.alta_calidad.replace("-min.webp", ".webp"); // Almacenamos la URL de alta calidad sin el "-min"
+        img.alt = foto.nombre;
+        img.onerror = () => imgError(img); // Asignar la función imgError al evento onerror
+
+        div.appendChild(img);
+        galeria.appendChild(div);
+      });
+    })
+    .catch(error => {
+      console.error('Error al cargar el archivo JSON:', error);
+    });
+};
+
+// Función para configurar la funcionalidad de la ventana modal y los botones de descarga
+const configurarModal = () => {
+  const modal = document.querySelector('.modal');
+  const modalImg = document.querySelector('.imagen');
+  const modalDownloadButton = document.querySelector('#download-button');
+  const modalDownloadOriginalButton = document.querySelector('#download-button2');
+  const closeBtn = document.querySelector('.close');
+
+  if (closeBtn) {
+    closeBtn.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+  }
+
+  modalDownloadButton.addEventListener('click', () => {
+    window.location.href = modalDownloadButton.href;
+  });
+
+  modalDownloadOriginalButton.addEventListener('click', () => {
+    window.location.href = modalDownloadOriginalButton.href;
+  });
+
+  const acordeones = document.querySelectorAll('.accordion-item');
+
+  acordeones.forEach(acordeon => {
+    acordeon.addEventListener('click', () => {
+      const images = acordeon.querySelectorAll(".photo img");
+      const captionText = acordeon.querySelector(".caption");
+
+      images.forEach((img) => {
+        img.addEventListener("click", function () {
+          modal.style.display = "block";
+
+          // Cargar la versión de alta calidad al hacer clic en la imagen
+          modalImg.src = this.dataset.highres; // Cambiamos la fuente de la imagen a la URL de alta calidad
+          modalDownloadButton.download = this.alt;
+          modalDownloadButton.href = this.dataset.highres;
+
+          const altaCalidadLink = this.dataset.highres;
+          const calidadOriginalLink = altaCalidadLink.replace("-min.webp", ".webp"); // Reemplazamos "-min.webp" por ".webp" para obtener el enlace de alta calidad
+          const jpgLink = calidadOriginalLink.replace(".webp", ".jpg");
+          modalDownloadOriginalButton.href = jpgLink;
+
+          captionText.innerHTML = this.alt;
+        });
+      });
+    });
+  });
+};
+
+
+// Llamada a la función para cargar fotos
+const json1 = 'https://autografia.github.io/fotos/campello_enero_2023/fotos.json';
+const galeria1 = document.querySelectorAll(".galeria")[0];
+const json2 = 'https://autografia.github.io/fotos/concentración_turron/fotos.json';
+const galeria2 = document.querySelectorAll(".galeria")[1];
+const json3 = 'https://autografia.github.io/fotos/volrace_gt_2022/fotos.json';
+const galeria3 = document.querySelectorAll(".galeria")[2];
+const json4 = 'https://autografia.github.io/fotos/sanjuan2023/fotos.json';
+const galeria4 = document.querySelectorAll(".galeria")[3];
+
+cargarFotos(json1, galeria1).then(configurarModal);
+cargarFotos(json2, galeria2).then(configurarModal);
+cargarFotos(json3, galeria3).then(configurarModal);
+cargarFotos(json4, galeria4).then(configurarModal);
